c5ccd5f3e03235d5b8360198875ba6abc395ee29
Aina Llaneras <aina.llaneras@estudiantat.upc.edu>
2024-05-22 12:27:19 +0200

    yey

--- Apartat1.R
+++ Apartat1.R
@@ -1,4 +1,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(ggplot2)
+library(tidyr)
+library(dplyr)
 
  #### Apartat 1 Flux elèctric òptim #### 
 
@@ -18,18 +21,17 @@ T <- 1:24
 df3 <- read.table("Apartat1_3.txt", header = TRUE)
 df3$T <- T
 df32 <- pivot_longer(df3, cols = c(PG_NRC3, PG_NRC1), names_to = "Generadors", values_to = "Potencia")
-library(ggplot2)
+
 graf1 <- ggplot(data = df32, aes(x = T, y = Potencia, group = Generadors)) 
 
 graf1 +
-  geom_line(aes(color=factor(Generadors)),size=1.5) +
+  geom_line(aes(color=factor(Generadors)),linewidth=1.1) +
   labs(title = "Perfil de potència",
        x = "T",
        y = "MGW",
        color = "Generador") 
 
 ## d) quin és l'arc més sobrecarregat? gràfic del seu perfil de potència al llarg del temps 
-
 
 pot <- matrix(c(1,2,289.897,
 1,5,839.888,
@@ -58,14 +60,36 @@ arc <- read.table("Apartat1_41.txt", header = TRUE)
 arc$T <- T
 
 ggplot(data = arc, aes(x = T, y = potencia))+ 
-  geom_line(size=1.5) +
+  geom_line(linewidth=1.1) +
   labs(title = "Perfil de potència arc (4,7)",
        x = "T",
        y = "MGW") 
 
 ## e) marge de potència disponible té el sistema a cada període per absorbir eventuals desajustos d oferta i o demanda
+marges <- read.table("Apartat_42.txt", header = TRUE)
 
+pg2 <- pg[,-which(sapply(pg, sum)==0)]
+pg2$T <- T
+marges$generador <- colnames(pg)
+marges <- marges[-which(sapply(pg, sum)==0),]
 
+potencia_hores <- pivot_longer(data = pg2, cols = 1:4, names_to = "generador", values_to = "mgw")
+
+prova <- left_join(marges[,3:5], potencia_hores, by="generador")
+prova[prova$rdown < 0, 2] <- 0
+
+ggplot(data = prova, aes(x = T, y = mgw)) + 
+  geom_ribbon(aes(ymin = rdown, ymax = rup), fill = "grey80", alpha = 0.5) +
+  geom_line() +
+  facet_wrap(~generador, ncol = 2) +
+  labs(x="T", 
+       y="MGW")+
+  geom_hline(data = marges, aes(yintercept = pmax), color = "red", linetype = "dashed") +
+  geom_hline(data = marges, aes(yintercept = pmin), color = "blue", linetype = "dashed") 
 
 ## f) creus que pot tenir setnit incrementar la capacitat d'alguna unitat generadora? si es així, quina inversió econòmica estaries disposat a fer?
+
+max_min_data <- potencia_hores %>%
+  group_by(generador) %>%
+  summarize(mgw_max = max(mgw), mgw_min = min(mgw))
 
