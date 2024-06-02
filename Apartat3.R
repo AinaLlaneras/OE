21346989360a8a95151b512eb1da07f6d1d21244
Aina Llaneras <aina.llaneras@estudiantat.upc.edu>
2024-06-02 16:57:14 +0200

    h

---
+++ Apartat3.R
@@ -0,0 +1,160 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+rm(list = ls())
+#### 3.1. Problema infactible ####
+
+# ampl: include E1A31.run;
+# presolve, variable P[7,8,21]:
+#   impossible deduced bounds: lower = 8.14, upper = 8;
+#   difference = 0.14
+#   presolve, variable P[7,8,14]:
+#     impossible deduced bounds: lower = 8.58, upper = 8;
+#   difference = 0.58
+#   presolve, variable P[7,8,13]:
+#     impossible deduced bounds: lower = 8.25, upper = 8;
+#   difference = 0.25
+#   presolve: constraint NPFE[11,21] cannot hold:
+#     body <= -75.57 cannot be >= -73; difference = -2.57
+#   presolve: constraint NPFE[11,15] cannot hold:
+#     body <= -73.04 cannot be >= -73; difference = -0.04
+#   13 presolve messages suppressed.
+#   CostTotal = 36961.1
+
+#### 3.2. Alpha ####
+
+# Alpha = 0.0886109
+
+#### 3.3. Cost adicional ####
+
+## 1. Taules de les variables de decisció 
+# CostTotal = 999243
+
+## Hem ajustat el valor màxim d'alpha a 0.09 perquè sinó donava problema infactible 
+
+P <- read.table("P3.txt", header = TRUE)
+pg <- read.table("Apartat3_5_PG.txt", header = TRUE)
+th <- read.table("Apartat3_5_TH.txt", header = TRUE)
+alpha <- read.table("Apartat3_1_alpha.txt", skip = 1)
+colnames(alpha) <- c("D03","D04","D05","D08","D09","D10","D11","D12","D13","D14")
+u <- read.table("Apartat3_5_u.txt", header = TRUE)
+v <- read.table("Apartat3_1_v.txt", header = TRUE)
+w <- read.table("Apartat3_1_w.txt", header = TRUE)
+arcs <- c("(1,2)", "(1,5)", "(2,3)", "(2,4)", "(2,5)", "(3,4)", "(4,5)", "(4,7)", "(4,9)", "(5,6)", "(6,11)", "(6,12)", "(6,13)", "(7,8)", "(7,9)", "(9,10)", "(9,14)", "(10,11)", "(12,13)", "(13,14)")
+colnames(P) <- arcs
+
+## b) unitats necesaries per satisfer la demanda dels nodes a cada periode de temps 
+
+df2 <- read.table("Apartat3_2.txt", skip = 1)
+colnames(df2) <- "nUnitats"
+df2$T <- 1:24
+ggplot(data = df2) +
+  geom_line(aes(x = T, y = nUnitats, color = "pink"), linewidth=1.5) +
+  labs(title = "Unitats enceses cada hora", x = "T", y = "Unitats") +
+  theme(legend.position = "none",plot.title = element_text(hjust = 0.5))
+
+## c) gràfic del perfil de potència i energia de dues unitats generadores al llarg del temps
+T <- 1:24
+df3 <- read.table("Apartat3_3.txt", header = TRUE)
+df3$T <- T
+df32 <- pivot_longer(df3, cols = c(PG_NRC3, PG_NRC1), names_to = "Generadors", values_to = "Potencia")
+
+df32$Tprev <- df32$T -1
+
+ggplot(data = df32) + 
+  geom_rect(aes(xmin = Tprev, xmax = T, ymin = 0, ymax = Potencia, fill = Generadors, color = Generadors), 
+            alpha = 0.7) +
+  geom_line(aes(x = (T-0.5), y = Potencia, color = factor(Generadors)), linewidth = 1.1) +
+  labs(title = "Perfil de potència de dels generadors NRC1 i NRC3", x = "T", y = "MGW") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+## d) quin és l'arc més sobrecarregat? gràfic del seu perfil de potència al llarg del temps 
+
+carregatotal <- colSums(abs(P))
+names(carregatotal) <- colnames(P)
+names(which.max((carregatotal))) #arc L=(4,7) més sobrecarregat 
+arc <- data.frame(potencia = P[, which.max((carregatotal))])
+arc$T <- T
+arc$Tprev <- T-1
+
+ggplot(data = arc) + 
+  geom_rect(aes(xmin = Tprev, xmax = T, ymin = 0, ymax = potencia, color = "black")) +
+  geom_line(aes(x = T-0.5, y = potencia))+
+  labs(title = "Perfil de potència arc (4,7)", x = "T", y = "MGW") +
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5))
+
+## e) marge de potència disponible té el sistema a cada període per absorbir eventuals desajustos d oferta i o demanda
+marges <- read.table("Apartat3_42.txt", header = TRUE)
+U <- marges$rup
+D <- marges$rdown
+M <- marges
+# marges de generadors oberts
+pg2 <- pg[,-which(sapply(pg, sum)==0)]
+pg2$T <- T
+marges$generador <- colnames(pg)
+marges <- marges[-which(sapply(pg, sum)==0),]
+rownames(marges) <- marges$generador
+
+# fem un df en format allargat per poder fer el ggplot b
+potencia_hores <- pivot_longer(data = pg2, cols = 1:4, names_to = "generador", values_to = "mgw")
+
+# unim els marges amb les dades de potencia i ajustem els marges per cada temps i generador 
+prova <- left_join(marges[,3:5], potencia_hores, by="generador")
+prova[prova$rdown < 0, 2] <- 0
+prova[,"rdown"] <- prova$mgw-prova$rdown
+prova[,"rup"] <- prova$mgw+prova$rup
+prova[prova[,"rdown"]<0,"rdown"] <- 0
+
+prova2 <- prova
+
+# ajustem els marges pq on siguin mes grans q pmin i mes petits q pmax 
+for(i in 1:nrow(prova)){
+  if(prova[i,"mgw"]>0 && prova[i, "rdown"]<marges[prova[i,"generador"],"pmin"]){
+    prova2[i, "rdown"] <- marges[prova[i,"generador"],"pmin"]
+  } 
+  else if(prova[i, "rup"]>=marges[prova[i,"generador"],"pmax"]){
+    prova2[i, "rup"] <- marges[prova[i,"generador"],"pmax"]
+  }
+}
+prova2[prova2$generador=="ROB2"& prova2$rup > 40, "rup"] <- 70
+
+# gràfic per cada generador obert
+ggplot(data = prova2, aes(x = T, y = mgw)) + 
+  geom_ribbon(aes(ymin = rdown, ymax = rup), fill = "tomato", alpha = 0.5) +
+  geom_line() +
+  facet_wrap(~generador, ncol = ) +
+  labs(title = "Marge de potència de cada generador", x="T", y="MGW")+
+  geom_hline(data = marges, aes(yintercept = pmax), color = "red", linetype = "dashed") +
+  geom_hline(data = marges, aes(yintercept = pmin), color = "blue", linetype = "dashed") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+# ara ho fem per a tots els generadors: capcitat del sistema
+total <- prova %>% group_by(T) %>% summarise(potencia = sum(mgw))
+up_total <- sum(U)
+down_total <- sum(D)
+
+total$up <- total$potencia + up_total#colSums(t(u)*U) # generadors oberts * rampa up
+total$down <- total$potencia - down_total#colSums(t(u)*D) # generadors oberts * rampa down
+pmax_total <- sum(M$pmax) # suma dels maxims
+pmin_total <- min(M$pmin) # minim dels minims
+
+total[total$down<pmin_total,"down"] <- pmin_total
+
+
+m <- c(pmax_total, pmin_total)
+
+ggplot(data = total, aes(x = T, y = potencia)) + 
+  geom_ribbon(aes(ymin = down, ymax = up), fill = "tomato", alpha = 0.5) +
+  geom_line() +
+  labs(title = "Marge de potència del sistema", x="T", y="MGW")+
+  geom_hline(aes(yintercept = pmax_total), color = "red", linetype = "dashed") +
+  geom_hline(aes(yintercept = pmin_total), color = "blue", linetype = "dashed") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+## f) creus que pot tenir setnit incrementar la capacitat d'alguna unitat generadora? si es així, quina inversió econòmica estaries disposat a fer? 
+
+# L unic generador que es veu limitat per la capacitat és el ROB2. La resta sempre estan per sota el màxim. 
+
+
